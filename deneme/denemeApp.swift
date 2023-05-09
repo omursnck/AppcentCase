17d69b6b7505065b4996ba22fb4c90ab14ba5ee7
omursnck <omrsnr12@icloud.com>
2023-05-09 18:54:11 +0300

    Initial Commit

---
+++ deneme/denemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  denemeApp.swift
+//  deneme
+//
+//  Created by Ömür Şenocak on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct denemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
